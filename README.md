dcaa548f77f2d029bf98169a33e1787b1c7a6bac
Yamin Shihab <yaminshihab123@gmail.com>
2023-04-24 13:36:23 -0600

    Added base Chip8 struct

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # biscuit8
 
-A horrible CHIP-8 emulator written in Rust. I guess biscuit is sometimes a synonym for chip, so that's why I guess.
+A horrible CHIP-8 emulator written in Rust. I guess biscuit is sometimes a synonym for chip (as in CHIP-8), so that's why it's named BISCUIT-8 maybe.
 
 (Still in progress)
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use biscuit8::help;
+use biscuit8::Chip8;
+use std::{env, fs};
 
 fn main() {
-    println!("{}", help());
+    let path = env::args().nth(1).expect("Please give a path to the ROM");
+    let rom = fs::read(path).expect("Failed to load file");
+    let chip8 = Chip8::new(&rom).expect("Failed to create emulator instance");
 }
